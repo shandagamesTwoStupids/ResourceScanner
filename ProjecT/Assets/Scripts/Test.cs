d83ba0dfb06c9929a5bbb85cd8892fa528bfe423
renlvda <1041962748@qq.com>
2018-09-09 18:26:49 +0800

    update

---
+++ ProjecT/Assets/Scripts/Test.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using System.IO;
+using System.Text;
+
+public class Test : MonoBehaviour {
+
+	[MenuItem("Tools/bianli")]
+	static void CheckSceneSetting(){
+		List<string> dirs = new List<string> ();
+		GetDirs (Application.dataPath, ref dirs);
+	}
+
+	private static void GetDirs(string dirPath, ref List<string> dirs){
+		foreach (string path in Directory.GetFiles(dirPath))
+		{
+			//获取所有文件夹中包含后缀为 .mat 的路径
+			if (System.IO.Path.GetExtension(path) == ".mat")
+			{
+				dirs.Add(path.Substring(path.IndexOf("Assets")));
+				char[] pathChar = path.Substring(path.IndexOf("Assets")).ToCharArray ();
+				char[] resultChar = new char[pathChar.Length];
+				Array.Reverse (pathChar);
+				int i = 0;
+				while (i < pathChar.Length && pathChar [i] != '\\') {
+					resultChar [i] = pathChar [i];
+					i++;
+				}
+				Array.Reverse (resultChar);
+				StringBuilder builder = new StringBuilder ();
+				for (int j = 0; j < resultChar.Length; j++) {
+					if (resultChar [j] != '\0') {
+						builder.Append (resultChar [j]);
+					}
+				}
+				Debug.Log (builder);
+				//Debug.Log(path.Substring(path.IndexOf("Assets")));
+				//Debug.Log (new string(pathChar).Trim());
+			}
+		}
+
+		if (Directory.GetDirectories(dirPath).Length > 0)  //遍历所有文件夹
+		{
+			foreach (string path in Directory.GetDirectories(dirPath))
+			{
+				GetDirs(path, ref dirs);
+			}
+		}
+
+	}
+}
---
+++ ProjecT/Assets/Scripts/TestChar.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TestChar : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		char[] char1 = new char[]{ '1', '2', '3', '4', '5' };
+		char[] char2 = new char[char1.Length];
+		int i = 0;
+		while (i < char1.Length && char1 [i] != '5') {
+			char2 [i] = char1 [i];
+			Debug.Log (char2[i]);
+			Debug.Log (char1[i]);
+			i++;
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
