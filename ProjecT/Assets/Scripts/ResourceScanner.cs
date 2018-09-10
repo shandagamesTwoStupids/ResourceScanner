ce1c0ca841603fc95eb674aa9a273140feaa0df3
胡家斌.Harry <hujiabin.harry@snda.com>
2018-09-10 14:51:27 +0800

    modified

--- ProjecT/Assets/Scripts/ResourceScanner.cs
+++ ProjecT/Assets/Scripts/ResourceScanner.cs
@@ -6,6 +6,7 @@ using UnityEditor;
 using UnityEditor.SceneManagement;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
+using System.Security.Cryptography;
 
 public class ResourceScanner : EditorWindow {
 
@@ -36,6 +37,7 @@ public class ResourceScanner : EditorWindow {
 		public string ResourceType;
 		public int ResourceUseCount;
 		public string UseSceneName;
+        public string ResourceMd5;
 	}
 	//保存未激活的gameobject信息
 	public class InActiveHolder {
@@ -55,19 +57,19 @@ public class ResourceScanner : EditorWindow {
     //用于保存的文件地址
     private string mSaveFolderPath = "D:/workspace/test/";
     //用于保存记录的文件
-    private string mSaveFileName = "default.csv";
+    //private string mSaveFileName = "default.csv";
     //显示宽度（每列）
     const float WIDTH = 100f;
     //用于保存滚动内容位置
-    private Vector2 mScrollPos = new Vector2(0, 0);
+    //private Vector2 mScrollPos = new Vector2(0, 0);
     //用于显示资源排序方向
-    private string mTypeSortStr = "资源类型";
-    private string mUseSortStr = "资源使用次数";
-    private const string UP = "↑";
-    private const string DOWN = "↓";
-    private bool mTypeSortOrder = true;
-    private bool mUseSortOrder = true;
-	#endregion
+    //private string mTypeSortStr = "资源类型";
+    //private string mUseSortStr = "资源使用次数";
+    //private const string UP = "↑";
+    //private const string DOWN = "↓";
+    //private bool mTypeSortOrder = true;
+    //private bool mUseSortOrder = true;
+    #endregion
 
     //绘制
     void OnGUI() {
@@ -132,7 +134,13 @@ public class ResourceScanner : EditorWindow {
             float progressStep = 1.0f / nameOfScenes.Length;
             for (int i = 0; i < nameOfScenes.Length; i++) {
                 EditorUtility.DisplayProgressBar("扫描中", nameOfScenes[i], progress += progressStep);
-                Scene scene = EditorSceneManager.OpenScene(mSceneFolderPath + nameOfScenes[i] + ".unity", OpenSceneMode.Single);
+                Scene scene;
+                try {
+                    scene = EditorSceneManager.OpenScene(mSceneFolderPath + nameOfScenes[i] + ".unity", OpenSceneMode.Single);
+                } catch (System.Exception e) {
+                    Debug.Log(e.Message);
+                    continue;
+                }
                 if (!scene.IsValid()) {
                     Debug.Log("加载失败，场景名：" + nameOfScenes[i]);
                     continue;
@@ -300,17 +308,17 @@ public class ResourceScanner : EditorWindow {
     }
 
     //添加资源
+    const string DEFAULT_PATH = "No Path";
     private void AddResource(Object resource, string name, string type, string sceneName) {
         if (resource == null)
             return;
         //获取资源地址作为key值
-        string path;
+        string path = AssetDatabase.GetAssetPath(resource);
         string key;
-        if(type == MATERIAL) {
+        if("".Equals(path)) {
             key = name;
-            path = "No Path";
+            path = DEFAULT_PATH;
         } else {
-            path = AssetDatabase.GetAssetPath(resource);
             key = path;
         }
         if (mResourceDict.ContainsKey(key)) {
@@ -318,13 +326,15 @@ public class ResourceScanner : EditorWindow {
             resourceHolder.ResourceUseCount++;
             //对于场景名，目前看来似乎没有需要全部都显示的必要，所以先不做了
         } else {
+            string md5 = path.Equals(DEFAULT_PATH) ? "" : MD5(path);
             ResourceHolder resourceHolder = new ResourceHolder {
                 ResourceObject = resource,
                 ResourcePath = path,
                 ResourceName = name,
                 ResourceType = type,
                 ResourceUseCount = 1,
-                UseSceneName = sceneName
+                UseSceneName = sceneName,
+                ResourceMd5 = md5
             };
             mResourceDict.Add(key, resourceHolder);
             mResourceList.Add(resourceHolder);
@@ -365,10 +375,14 @@ public class ResourceScanner : EditorWindow {
 			//Renderer
 			Renderer[] renderers = sceneObjects [i].GetComponentsInChildren<Renderer> (true);
 			for (int j = 0; j < renderers.Length; j++) {
-				//material
-				if (renderers [j].materials != null) {
-					Material[] rendererMaterials = renderers [j].materials;
+                //material
+                Renderer renderer = PrefabUtility.GetPrefabParent(renderers[j]) as Renderer;
+                if (renderer != null && renderer.sharedMaterials != null) {
+					Material[] rendererMaterials = renderer.sharedMaterials;
 					for (int k = 0; k < rendererMaterials.Length; k++) {
+                        if(rendererMaterials[k] == null) {
+                            Debug.Log("Missing material! objName:" + renderer.gameObject.name);
+                        }
 						AddResource (rendererMaterials [k], rendererMaterials [k].name, MATERIAL, targetScene.name);
 						AddMaterialTextureResource (rendererMaterials [k], targetScene);
 					}
@@ -392,10 +406,11 @@ public class ResourceScanner : EditorWindow {
 				if (images [j].sprite != null) {
 					AddResource (images [j].sprite, images [j].sprite.name, SPRITE, targetScene.name);
 				}
-				//material
-				if (images [j].material != null) {
-					AddResource (images [j].material, images [j].material.name, MATERIAL, targetScene.name);
-					AddMaterialTextureResource (images [j].material, targetScene);
+                //material
+                Image image = PrefabUtility.GetPrefabParent(images[j]) as Image;
+                if (image != null && image.material != null) {
+					AddResource (image.material, image.material.name, MATERIAL, targetScene.name);
+					AddMaterialTextureResource (image.material, targetScene);
 				}
 			}
 
@@ -425,9 +440,11 @@ public class ResourceScanner : EditorWindow {
 			//Mesh Filter
 			MeshFilter[] meshFilters = sceneObjects [i].GetComponentsInChildren<MeshFilter> (true);
 			for (int j = 0; j < meshFilters.Length; j++) {
-				//mesh
-				if (meshFilters [j].mesh != null) {
-					AddResource (meshFilters [j].mesh, meshFilters [j].mesh.name, MESH, targetScene.name);
+                //mesh
+                MeshFilter meshFilter = PrefabUtility.GetPrefabParent(meshFilters[j]) as MeshFilter;
+				if (meshFilter != null && meshFilter.sharedMesh != null) {
+                    Mesh mesh = meshFilter.sharedMesh;
+                    AddResource (mesh, mesh.name, MESH, targetScene.name);
 				}
 			}
 
@@ -468,13 +485,7 @@ public class ResourceScanner : EditorWindow {
 	#region 文件处理
 	//保存资源到目标文件目录的文件，会对文件进行编号，对于已经存在的文件，会迭代编号创建新文件
 	private bool SaveResToFile(string folderPath, string fileName) {
-		int count = 0;
-		string partPath = folderPath + fileName + "_resource_";
-		string path = partPath;
-		do {
-			count++;
-			path = partPath + count.ToString() + ".csv";
-		} while (File.Exists(path));
+		string path = folderPath + fileName + "_resource_" + System.DateTime.Now.ToString("yy_MM_dd") + ".csv";
 		using (StreamWriter textWriter = new StreamWriter(path, false, System.Text.Encoding.Default)) {
 			string text = EncodeResourceList();
 			textWriter.Write(text);
@@ -494,14 +505,8 @@ public class ResourceScanner : EditorWindow {
 
 	//同时保存资源使用情况和对象激活情况
 	private bool SaveObjToFile(string folderPath, string fileName) {
-		int count = 0;
-		string partPath = folderPath + fileName + "_object_";
-		string objPath = partPath;
-		do {
-			count ++;
-            objPath = partPath + count.ToString() + ".csv";
-		} while (File.Exists(objPath));
-		using (StreamWriter objTextWriter = new StreamWriter(objPath, false, System.Text.Encoding.Default)) {
+        string path = folderPath + fileName + "_object_" + System.DateTime.Now.ToString("yy_MM_dd") + ".csv";
+		using (StreamWriter objTextWriter = new StreamWriter(path, false, System.Text.Encoding.Default)) {
 			string objText = EncodeObjList();
 			objTextWriter.Write(objText);
 		}
@@ -520,22 +525,25 @@ public class ResourceScanner : EditorWindow {
     //数据编码解码
     private string EncodeResourceList() {
         System.Text.StringBuilder stringBuilder = new System.Text.StringBuilder();
-        stringBuilder.Append("资源路径").Append(',')
-            .Append("资源名称").Append(',')
+        stringBuilder.Append("资源名称").Append(',')
+            .Append("资源路径").Append(',')
             .Append("资源类型").Append(',')
             .Append("资源被使用场景").Append(',')
-            .Append("资源使用次数").Append('\n');
+            .Append("资源使用次数").Append(',')
+            .Append("资源md5").Append('\n');
         for (int i = 0; i < mResourceList.Count; i++) {
-            stringBuilder.Append(mResourceList[i].ResourcePath).Append(',')
-                .Append(mResourceList[i].ResourceName).Append(',')
+            stringBuilder.Append(mResourceList[i].ResourceName).Append(',')
+                .Append(mResourceList[i].ResourcePath).Append(',')
                 .Append(mResourceList[i].ResourceType).Append(',')
                 .Append(mResourceList[i].UseSceneName).Append(',')
-                .Append(mResourceList[i].ResourceUseCount).Append('\n');
+                .Append(mResourceList[i].ResourceUseCount).Append(',')
+                .Append(mResourceList[i].ResourceMd5).Append('\n');
         }
         stringBuilder.Remove(stringBuilder.Length - 1, 1);
         return stringBuilder.ToString();
     }
 
+    //暂时被弃用了，如要启用需要参照编码修改
     private void DecodeResourceList(string text) {
         string[] allResourceStrs = text.Split('\n');
         for (int i = 1; i < allResourceStrs.Length; i++) {
@@ -609,10 +617,10 @@ public class ResourceScanner : EditorWindow {
 	private void ClearResourceList() {
 		mResourceDict.Clear();
 		mResourceList.Clear();
-		mTypeSortStr = "资源类型";
-		mUseSortStr = "资源使用次数";
-		mUseSortOrder = true;
-		mTypeSortOrder = true;
+		//mTypeSortStr = "资源类型";
+		//mUseSortStr = "资源使用次数";
+		//mUseSortOrder = true;
+		//mTypeSortOrder = true;
 		mInActiveList.Clear ();
 	}
 
@@ -656,4 +664,23 @@ public class ResourceScanner : EditorWindow {
 		}
 	}
 	#endregion
+
+    //MD5计算
+    public static string MD5(string filePath) {
+        try {
+            MD5CryptoServiceProvider md5Provider = new MD5CryptoServiceProvider();
+            using (FileStream file = new FileStream(filePath, FileMode.Open)) {
+                byte[] retVal = md5Provider.ComputeHash(file);
+                System.Text.StringBuilder sb = new System.Text.StringBuilder();
+                for (int i = 0; i < retVal.Length; i++) {
+                    sb.Append(retVal[i].ToString("x2"));
+                }
+                return sb.ToString();
+            }
+        }
+        catch (System.Exception ex) {
+            Debug.LogError("Compute md5 failed, file path: " + filePath + ", Exception massage: " + ex.Message);
+            return "";
+        }
+    }
 }
